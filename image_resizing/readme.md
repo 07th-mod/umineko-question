6fba722b64a4a795d0b2130c69791c50ca2ed2aa
Daniel Wong <drojfjord@gmail.com>
2018-04-13 23:41:34 +1000

    Add image resizing scripts (to go from ps3 sprites to Question Arc usable sprites)

---
+++ image_resizing/readme.md
@@ -0,0 +1,3 @@
+Use xnview to run the .xbs image resizing script. I had to fiddle with the settings a bit to get it to work (depending on how resizing/cropping is done, it can be 1 pixel off)
+
+Note that one of rosa's sprites had to be manually edited to add the sweat bead.
---
+++ image_resizing/rename_rosa.bat
@@ -0,0 +1,16 @@
+copy ros_c12_akuwarai1.png ros_akuwaraia1.png
+copy ros_c12_aseru1.png ros_aserua1.png
+copy ros_c12_def1.png ros_defa1.png
+copy ros_c22_ikari1.png ros_ikaria1.png
+copy ros_c22_ikari2.png ros_ikaria2.png
+copy ros_c11_ikari3.png ros_ikaria3.png
+copy ros_c32_ikari4.png ros_ikaria4.png
+copy ros_c12_komaru1.png ros_komarua1.png
+:: special case ros_komarua2.png - manually remove sweat bead from  ros_c12_komaru4.png
+copy ros_c12_komaru3.png ros_komarua3.png
+copy ros_c12_komaru4.png ros_komarua4.png
+copy ros_c11_majime1.png ros_majimea1.png
+copy ros_c11_nayamu1.png ros_nayamua1.png
+copy ros_c22_odoroki1.png ros_odorokia1.png
+copy ros_c12_warai1.png ros_waraia1.png
+copy ros_c12_warai2.png ros_waraia2.png
