d3aad39e956ae608ac5db67a521482e2225d5c73
Naoki <eigoninaritai.naokichi@gmail.com>
2018-09-03 15:45:05 +0900

    Eliminated all exceptions.

--- tools/VoicesPuter/VoicesPuter/Program.cs
+++ tools/VoicesPuter/VoicesPuter/Program.cs
@@ -29,115 +29,14 @@ namespace VoicesPuter
             // Read all of the game script lines.
             string gameScriptPath = args[0];
             ChangedGameScriptMaker changedGameScriptMaker = new ChangedGameScriptMaker(gameScriptPath);
-            List<string> gameScriptLines;
-            try
-            {
-                gameScriptLines = changedGameScriptMaker.ReadGameScript();
-            }
-            catch (ArgumentException argumentNullProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw argumentNullProblem;
-            }
-            catch (DirectoryNotFoundException directoryNotFoundProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw directoryNotFoundProblem;
-            }
-            catch (FileNotFoundException fileNotFoundProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw fileNotFoundProblem;
-            }
-            catch (NotSupportedException notSupportedProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw notSupportedProblem;
-            }
-            catch (Exception otherProblem)
-            {
-                // TODO Have to log about the error.
-                Console.WriteLine("Occured unexpected error.");
-                throw otherProblem;
-            }
+            List<string> gameScriptLines = changedGameScriptMaker.ReadGameScript();
 
             // Put voice scripts into Japanese line and change voice script's function name of both.
-            List<string> changedGameScriptLines;
-            VoicesPuter voicesPuter = new VoicesPuter();
-            try
-            {
-                changedGameScriptLines = voicesPuter.PutVoiceScriptsIntoLines(gameScriptLines);
-            }
-            catch (UnmatchedNewLinesWithOriginalLinesException unmatchedNewLinesWithOriginalLinesProbelem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw unmatchedNewLinesWithOriginalLinesProbelem;
-            }
-            catch (Exception otherProblem)
-            {
-                // TODO Have to log about the error.
-                Console.WriteLine("Occured unexpected error.");
-                throw otherProblem;
-            }
+            VoicesPuter voicesPuter = new VoicesPuter(gameScriptPath);
+            List<string> changedGameScriptLines = voicesPuter.PutVoiceScriptsIntoLines(gameScriptLines);
 
             // Make the changed game script into output directory.
-            try
-            {
-                changedGameScriptMaker.MakeChangedGameScript(changedGameScriptLines);
-            }
-            catch (SecurityException securityProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw securityProblem;
-            }
-            catch (UnauthorizedAccessException unauthorizedAccessProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw unauthorizedAccessProblem;
-            }
-            catch (ArgumentException argumentNullProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw argumentNullProblem;
-            }
-            catch (DirectoryNotFoundException directoryNotFoundProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw directoryNotFoundProblem;
-            }
-            catch (PathTooLongException pathTooLongProbelm)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw pathTooLongProbelm;
-            }
-            catch (IOException ioProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw ioProblem;
-            }
-            catch (ObjectDisposedException objectDisposedProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw objectDisposedProblem;
-            }
-            catch (NotSupportedException notSupportedProblem)
-            {
-                // TODO Have to notify the problem occured in command line.
-                // TODO Have to log about the error.
-                throw notSupportedProblem;
-            }
+            changedGameScriptMaker.MakeChangedGameScript(changedGameScriptLines);
             Console.WriteLine("Completed putting voice scripts into Japanese lines.");
             Console.ReadKey();
         }
