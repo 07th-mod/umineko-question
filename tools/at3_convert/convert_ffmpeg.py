5d8a082c9ce2ec6a14dc75b2cb379433c531608b
Daniel Wong <drojfjord@gmail.com>
2023-03-13 13:19:47 +1100

    Improve at3 conversion script

--- tools/at3_convert/convert_ffmpeg.py
+++ tools/at3_convert/convert_ffmpeg.py
@@ -11,11 +11,22 @@ import os
 # Sample Rate: 44100hz
 # Channels: 2 (Answer arcs uses 1. It shouldn't make any difference, but just to keep it consistent should use 2 for question arcs)
 
+num_channels = '2'
+
 output_folder = 'output'
 os.makedirs(output_folder, exist_ok=True)
 
 for path in glob.glob('./input/*.at3'):
     path = Path(path)
-    output_path = Path(output_folder).joinpath(Path(path.name).with_suffix('.ogg'))
+    temp = Path(output_folder).joinpath(Path(path.name).with_suffix('.wav'))
+
+    # The length will be incorrect for certain .at3 files if converting directly to .ogg
+    # To get around this, convert to .wav first, then conver the .wav to .ogg
     print(f'Converting {path}')
-    subprocess.check_call(['ffmpeg', '-i', path, '-ar', '44100', '-ac', '2', output_path])
+    subprocess.check_call(['ffmpeg', '-i', path, '-ar', '44100', '-ac', num_channels, temp])
+
+    dst = Path(temp).with_suffix('.ogg')
+
+    subprocess.check_call(['ffmpeg', '-y', '-i', temp, '-ar', '44100', '-ac', num_channels, '-q', '8', dst])
+
+    os.remove(temp)
